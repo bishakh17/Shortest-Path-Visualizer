4abd4b8beb4d87bfb59602567781e2f23cff26d9
bishakh17 <71015419+bishakh17@users.noreply.github.com>
2022-07-03 06:02:17 +0530

    Heuristic function changed from manhattan distance to euclidian distance

--- algo.py
+++ algo.py
@@ -5,7 +5,7 @@ from queue import PriorityQueue
 def h(p1, p2):
 	x1, y1 = p1
 	x2, y2 = p2
-	return abs(x1 - x2) + abs(y1 - y2)
+	return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)
 
 
 def reconstruct_path(came_from, current, draw):
